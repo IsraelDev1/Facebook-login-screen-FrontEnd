ad34b3a11fe0c48f8849b00157e3ac7d740ac496
Israel Alves <ia.nicolau@outlook.com>
2020-11-19 18:51:37 -0300

    Header ready and body started

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login facebook</title>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Lato&family=Roboto&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+	<header>
+		<div class="center">
+			<div class="logo">
+				<h2>Facebook</h2>	
+			</div><!-- logo -->
+
+			<form method="post" class="form-login"/>
+				<div class="form-element">
+					<p>E-mail ou telefone</p>
+					<input type="email">
+				</div><!-- form-element -->
+				<div class="form-element">
+					<p>Senha:</p>
+					<input type="password">
+				</div><!-- form-element -->
+				<div class="form-element">
+					<input type="submit" name="acao" value="Enviar">
+				</div><!-- form-element -->
+			</form><!-- form-login -->
+			<div class="clear"></div>
+		</div><!-- center -->
+	</header>
+
+	<section class="main">
+		<div class="center">
+			<div class="img-pessoas">
+				<img src="imagens/img1.png">
+			</div>
+
+			<div class="clear"></div>
+		</div><!-- center -->
+	</section><!-- main -->
+
+</body>
+</html>
