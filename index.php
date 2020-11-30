a0a1fcfbf9de0ddb0a12bf8e2812c078f4448a8b
Israel Alves <ia.nicolau@outlook.com>
2020-11-30 12:28:10 -0300

    Body ready

--- index.php
+++ index.php
@@ -37,6 +37,32 @@
 				<img src="imagens/img1.png">
 			</div>
 
+			<div class="abrir-conta">
+				<h2>Abra sua conta</h2>
+				<h3>É gratuito e sempre será!</h3>
+
+				<form class="criar-conta">
+					<div class="w50">
+						<input placeholder="Nome" type="text">
+					</div><!-- w50 -->
+					<div class="w50">
+						<input placeholder="Sobrenome" type="text">
+					</div><!-- w50 -->
+					<div class="w100">
+						<input placeholder="Email" type="email">
+					</div><!-- w100 -->
+					<div class="w100">
+						<input placeholder="Senha" type="password">
+					</div><!-- w100 -->
+
+					<div class="w100">
+						<input type="submit" name="acao" value="Cadastrar">
+					</div><!-- w100 -->
+
+					<div class="clear"></div>
+				</form><!-- criar-conta -->
+			</div><!-- abrir-conta -->
+
 			<div class="clear"></div>
 		</div><!-- center -->
 	</section><!-- main -->
